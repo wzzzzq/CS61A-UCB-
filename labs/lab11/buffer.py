f178d5a61d417d1781e1f1150077972aa4a0e0a4
Wang Ziqian <ChristianWang@sjtu.edu.cn>
2025-02-12 20:38:22 +0800

    ants completed

--- labs/lab11/buffer.py
+++ labs/lab11/buffer.py
@@ -1,51 +1,161 @@
-class Buffer(object):
-    """A Buffer provides a way of accessing a sequence one at a time.
-    Its constructor takes a sequence, called the "source".
-    The Buffer supplies elements from source one at a time through its pop_first()
-    method. In addition, Buffer provides a current() method to look at the
-    next item to be supplied, without moving past it.
-    >>> buf = Buffer(['(', '+', 15, 12, ')'])
-    >>> buf.pop_first()
+"""The buffer module assists in iterating through lines and tokens."""
+
+import math
+import sys
+
+
+class EOL_TOKEN:
+    """
+    A token that represents the end of a line in the Buffer's input source.
+    """
+
+    def __repr__(self):
+        return "This is a token representing the end of a line."
+
+
+# The following line of code makes a single instance of the class EOL_TOKEN.
+# This single instance is what you will be referencing as EOL_TOKEN in Buffer.
+EOL_TOKEN = EOL_TOKEN()
+
+
+class Buffer:
+    """A Buffer provides a way of accessing a sequence of tokens across lines.
+
+    Its constructor takes an iterator, called "the source", that returns the
+    next line of tokens as a list each time it is queried, or None to indicate
+    the end of data.
+
+    The Buffer in effect concatenates the sequences returned from its source
+    and then supplies the items from them one at a time through its pop_first()
+    method, calling the source for more sequences of items only when needed.
+
+    In addition, Buffer provides a current instance attribute to look at the
+    next item to be supplied, without sequencing past it.
+
+    >>> buf = Buffer(iter([['(', '+'], [15], [], [12, ')']]))
+    >>> buf.end_of_line()   # False since we have not reached the end of a line
+    False
+    >>> buf.current
     '('
     >>> buf.pop_first()
+    '('
+    >>> buf.current
     '+'
-    >>> buf.current()
+    >>> buf.pop_first()
+    '+'
+    >>> buf.end_of_line()   # We have reached the end of a line
+    True
+    >>> buf.current
+    This is a token representing the end of a line.
+    >>> buf.pop_first()
+    This is a token representing the end of a line.
+    >>> buf.current # Move onto the next line
     15
     >>> buf.pop_first()
     15
-    >>> buf.current()
+    >>> buf.current
+    This is a token representing the end of a line.
+    >>> buf.pop_first()
+    This is a token representing the end of a line.
+    >>> buf.current # This should be EOL_TOKEN, since this line is empty
+    This is a token representing the end of a line.
+    >>> buf.end_of_line()
+    True
+    >>> buf.pop_first()
+    This is a token representing the end of a line.
+    >>> buf.current
     12
     >>> buf.pop_first()
     12
+    >>> buf.current
+    ')'
     >>> buf.pop_first()
     ')'
-    >>> buf.pop_first()  # returns None
+    >>> buf.current
+    This is a token representing the end of a line.
+    >>> buf.pop_first()
+    This is a token representing the end of a line.
+    >>> buf.current         # returns None
+    >>> buf.pop_first()     # returns None
     """
+
     def __init__(self, source):
-        self.index = 0
-        self.source = source
+        """
+        Initialize a Buffer instance based on the given source.
+        """
+
+        # BEGIN
+        "*** YOUR CODE HERE ***"
+        self.gen=self.create_generator(source)
+        self.current=next(self.gen,None)
+        # END
+
+    def create_generator(self, source):
+        """
+        Yield tokens from the source. At the end of every line of source input,
+        yield EOL_TOKEN.
+        """
+        # BEGIN
+        "*** YOUR CODE HERE ***"
+        for lines in source:
+            for elem in lines:
+                yield elem
+            yield EOL_TOKEN
+        # END
 
     def pop_first(self):
-        """Remove the next item from self and return it. If self has
-        exhausted its source, returns None."""
-        current = self.current()
-        self.index += 1
-        return current
+        """
+        Return the current token from self, and update the current token to
+        be the next token. If there are no more tokens in the source, update
+        the current token to be None.
+        """
+        # BEGIN
+        "*** YOUR CODE HERE ***"
+        cur=self.current
+        self.current=next(self.gen,None)
+        return cur
+        # END
 
-    def current(self):
-        """Return the current element, or None if none exists."""
-        if self.index >= len(self.source):
-            return None
-        else:
-            return self.source[self.index]
+    def end_of_line(self):
+        return self.current is EOL_TOKEN
 
-    def expect(self, expected):
-        actual = self.pop_first()
-        if expected != actual:
-            raise SyntaxError("expected '{}' but got '{}'".format(expected, actual))
-        else:
-            return actual
+    def has_more(self):
+        return self.current is not None
 
-    def __str__(self):
-        return str(self.source[self.index:])
 
+# Try to import readline for interactive history
+try:
+    import readline
+except:
+    pass
+
+
+class InputReader:
+    """An InputReader is an iterable that prompts the user for input."""
+
+    def __init__(self, prompt):
+        self.prompt = prompt
+
+    def __iter__(self):
+        while True:
+            yield input(self.prompt)
+            self.prompt = ' ' * len(self.prompt)
+
+
+class LineReader:
+    """A LineReader is an iterable that prints lines after a prompt."""
+
+    def __init__(self, lines, prompt, comment=";"):
+        self.lines = lines
+        self.prompt = prompt
+        self.comment = comment
+
+    def __iter__(self):
+        while self.lines:
+            line = self.lines.pop(0).strip('\n')
+            if (self.prompt is not None and line != "" and
+                not line.lstrip().startswith(self.comment)):
+                print(self.prompt + line)
+                self.prompt = ' ' * len(self.prompt)
+            yield line
+        raise EOFError
--- labs/lab12/lab12.scm
+++ labs/lab12/lab12.scm
@@ -1,32 +1,93 @@
+; City Abstraction
+(define (make-city name lat lon)
+  (cons name (cons lat (cons lon nil))))
 
-(define-macro (def func args body)
-    'YOUR-CODE-HERE
-    `(define ,(cons func args) ,body)    
-)
+(define (get-name city) (car city))
 
-(define (map-stream f s)
-    (if (null? s)
-    	nil
-    	(cons-stream (f (car s)) (map-stream f (cdr-stream s)))))
+(define (get-lat city) (car (cdr city)))
 
+(define (get-lon city) (car (cdr (cdr city))))
 
-(define all-three-multiples
-  (map-stream (lambda (x) (+ 3 x)) (cons-stream 0 all-three-multiples))
-)
+(define (distance city-a city-b) 'YOUR-CODE-HERE)
 
-(define (compose-all funcs)
-  'YOUR-CODE-HERE
-  (if (null? funcs) (lambda (x) x)
-      (lambda (x) ((compose-all (cdr funcs)) ((car funcs) x)))
-  )
-)
+(define (closer-city lat lon city-a city-b)
+  'YOUR-CODE-HERE)
 
-(define (helper ps s)
-    (if (null? s) nil
-    (cons-stream (+ ps (car s)) (helper (+ ps (car s)) (cdr-stream s)))
-))
-(define (partial-sums stream)
-  'YOUR-CODE-HERE
-  (helper 0 stream)
-)
+; Teacher and Student Abstractions
+(define (student-create name classes)
+  (cons name classes))
 
+(define (teacher-create name class students)
+  (cons name (cons class students)))
+
+(define (student-get-name student)
+  'YOUR-CODE-HERE)
+
+(define (student-get-classes student)
+  'YOUR-CODE-HERE)
+
+(define (teacher-get-name teacher)
+  'YOUR-CODE-HERE)
+
+(define (teacher-get-class teacher)
+  'YOUR-CODE-HERE)
+
+(define (teacher-get-students teacher)
+  'YOUR-CODE-HERE)
+
+(define (student-attend-class student class)
+  'YOUR-CODE-HERE)
+
+(define (teacher-hold-class teacher)
+  'YOUR-CODE-HERE)
+
+; Rational Abstraction
+; Helpers
+(define (cadr lst) (car (cdr lst)))
+
+(define (gcd a b)
+  (if (= b 0)
+      (abs a)
+      (gcd b (modulo a b))))
+
+; Creates an object representing the rational number n/d
+; Assume: n, d are integers and d != 0
+; Note that this constructor simplifies the numerator and denominator
+(define (rational n d)
+  (let ((g (if (> d 0)
+               (gcd n d)
+               (- (gcd n d)))))
+    (list (/ n g) (/ d g))))
+
+; Gets the numerator of rational number r
+(define (numer r) (car r))
+
+; Gets the denominator of rational number r
+(define (denom r) (cadr r))
+
+; Adds two rational numbers x and y
+(define (add-rational x y)
+  (let ((new-num (+ (* (numer x) (denom y))
+                    (* (numer y) (denom x))))
+        (new-den (* (denom x) (denom y))))
+    (rational new-num new-den)))
+
+; Multiply two rational numbers x and y
+(define (mul-rational x y)
+  (let ((new-num (* (numer x) (numer y)))
+        (new-den (* (denom x) (denom y))))
+    (rational new-num new-den)))
+
+; Tree Abstraction
+; Constructs tree given label and list of branches
+(define (tree label branches)
+  (cons label branches))
+
+; Returns the label of the tree
+(define (label t) (car t))
+
+; Returns the list of branches of the given tree
+(define (branches t) (cdr t))
+
+; Returns #t if t is a leaf, #f otherwise
+(define (is-leaf t) (null? (branches t)))
